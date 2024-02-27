ad62575ca933bcdf28b9ebdb081b90c0d9c7af26
Albert499 <a.logozaga@alustudent.com>
2024-02-27 14:30:52 -0800

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 90):
-    if i % 10 > i / 10:
-        if i != 89:
-            print("{:02d}, ".format(i), end='')
-        else:
-print("{:02d}".format(i))
+for i in range(10):
+    for j in range(10):
+        print(f"{i}{j}", end=" ")
+    print()
