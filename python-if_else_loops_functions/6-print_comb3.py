c74f0d97ccfa9111e2c1d85013da659a376c2dd8
Albert499 <a.logozaga@alustudent.com>
2024-02-27 06:53:57 -0800

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for first_number in range(0, 10):
-    for second_number in range(first_number + 1, 10):
-        if first_number == 8 and second_number == 9:
-            print("{}{}".format(first_number, second_number))
-        else:
-            print("{}{}, ".format(first_number, second_number), end='')
+for num1 in range(10):
+    for num2 in range(10):
+        if num2 > num1 and num1 != 8:
+            print("{}{}".format(num1, num2), end=", ")
+        elif num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+            break
 ~
 ~
 ~
