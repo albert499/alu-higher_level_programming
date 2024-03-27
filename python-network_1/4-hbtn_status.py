d996fef8d3edb721fcd7f566d68daa46a94724ab
alberta logozaga <a.logozaga@alustudent.com>
2024-03-27 10:41:50 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-script that fetches https://alu-intranet.hbtn.io/status.
-"""
 import requests
-
 
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
-    content = response.text
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
