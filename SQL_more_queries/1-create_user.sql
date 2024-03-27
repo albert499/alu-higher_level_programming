4c971d7a68af0f4352a6f8f2ff4f34d6569a3e30
alberta logozaga <a.logozaga@alustudent.com>
2024-03-27 01:44:26 +0000

    All fixed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
--- creates MySQL server user `user_0d_1`
--- should have all privileges
--- password - `user_0d_1_pwd`
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+-- Create user 'user_0d_1' with all privileges
+-- Password for user set as 'user_0d_1_pwd'
+-- If user already exists, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
