e60c275f2ad6cab0d499ee2182321d3a0d460d1e
alberta logozaga <a.logozaga@alustudent.com>
2024-03-27 01:26:31 +0000

    All fixed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- script 1
--- creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+-- creates a user and password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
+FLUSH PRIVILEGES;
