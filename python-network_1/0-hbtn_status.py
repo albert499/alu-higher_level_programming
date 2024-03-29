df02dbf08c68b957764cd086ab5475991812065e
alberta logozaga <a.logozaga@alustudent.com>
2024-03-29 09:57:14 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"Module that fetches https://alu-intranet.hbtn.io/status"
-import urllib.request
+""" Fetches basic request with urlib"""
+from urllib import request
 
-
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+with request.urlopen("https://intranet.hbtn.io/status") as response:
+    r = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(r), r, r.decode('utf-8')))
