1be7074e382b861de8205893748e46350f8d1aea
alberta logozaga <a.logozaga@alustudent.com>
2024-04-04 11:26:52 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-'''
-Script that fetches https://intranet.hbtn.io/status
-'''
-from urllib import request
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
+"""
+import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-with request.urlopen(url) as response:
-    the_page = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(the_page)))
-    print('\t- content: {}'.format(the_page))
-    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
